0a80d3974dc5fec8cbed94a40bfef6a55ffbd0d8
Paulo <102394018+P1ul0@users.noreply.github.com>
2023-05-22 20:45:34 -0300

    Delete dist directory

--- dist/server.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var app_1 = require("./app");
-var server = new app_1.App();
-server.start(3000);
-//# sourceMappingURL=server.js.map
